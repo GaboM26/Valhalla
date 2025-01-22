4da0f4069652bcb37ce2a506be878114c9462939
Gabriel Millares Bellido <gabomillaresb@gmail.com>
2025-01-22 01:15:42 -0500

    adding main driver client and executin from main function

---
+++ src/valhalla/clients/driver_client.py
@@ -0,0 +1,9 @@
+from src.valhalla.clients.mysql_client import PyMySqlClient
+
+class DriverClient:
+
+    def __init__():
+        pass
+
+    def run():
+        pass
---
+++ src/valhalla/main.py
@@ -0,0 +1,11 @@
+from src.valhalla.clients.driver_client import DriverClient
+
+def main():
+    driver = DriverClient()
+
+    # Other Logic (secret retrieval, etc...)
+
+    driver.run()
+
+if __name__ == "__main__":
+    main()
