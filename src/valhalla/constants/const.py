9764c636f86d9beeaa01ec628a4b56c7fed14676
Gabriel Millares Bellido <gabomillaresb@gmail.com>
2025-01-23 23:17:45 -0500

    adding better descriptions for parameters passed in

--- src/valhalla/constants/const.py
+++ src/valhalla/constants/const.py
@@ -1,1 +1,3 @@
 DEFAULT= "DEFAULT"
+DECRYPT = "dec"
+ENCRYPT = "enc"
--- src/valhalla/main.py
+++ src/valhalla/main.py
@@ -8,13 +8,12 @@ sys.path.append(project_root)
 
 from src.valhalla.clients.driver_client import DriverClient
 from src.valhalla.utils.parser import ValhallaConfigParser
-from src.valhalla.constants.const import DEFAULT
+from src.valhalla.constants.const import *
 
 def parse_config():
     config_parser = ValhallaConfigParser()
 
-def main():
-
+def main(argv):
     config = parse_config()
     driver = DriverClient()
 
@@ -23,9 +22,10 @@ def main():
     driver.run()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Valhalla Command Line Interface")
+    parser = argparse.ArgumentParser(description="Welcome to Project Valhalla")
     parser.add_argument('--config', type=str, default=DEFAULT, help='Path to secrets file')
-    parser.add_argument('--run-mode', type=str, choices=['default', 'custom'], default='default', help='Mode to run the driver client')
+    parser.add_argument('--run-mode', type=str, choices=['enc', 'dec', 'ls'], default=DECRYPT, help='Mode \
+                        to run the driver client')
     
     args = parser.parse_args()
-    main()
+    main(args)
